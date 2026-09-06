dfbb29b4fec75b0e2ba5f432339d2789ec940ad7
root <root@codex.air12>
2026-09-06 01:10:06 -0400

    Modernize theme with image building badges

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "zroute-planner-v3";
-const ASSETS = ["./", "./index.html", "./i18n.js", "./manifest.webmanifest", "./icon.svg", "./data/progression.json", "./data/resources.json"];
+const CACHE = "zroute-planner-v4";
+const ASSETS = ["./", "./index.html", "./i18n.js", "./assets/buildings/research.svg", "./assets/buildings/military.svg", "./assets/buildings/defense.svg", "./assets/buildings/medical.svg", "./assets/buildings/economy.svg", "./assets/buildings/default.svg", "./manifest.webmanifest", "./icon.svg", "./data/progression.json", "./data/resources.json"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil((async () => {
   await Promise.all((await caches.keys()).filter(key => key.startsWith("zroute-planner-") && key !== CACHE).map(key => caches.delete(key)));
