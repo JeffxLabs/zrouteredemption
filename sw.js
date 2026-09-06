69f5e27ce6a846f43f938feca7076bb0627694b2
root <root@codex.air12>
2026-09-06 01:58:04 -0400

    Add missing Warrior Centre icon and precache it

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "zroute-planner-v9";
-const ASSETS = ["./", "./index.html", "./i18n.js", "./assets/buildings/research.svg", "./assets/buildings/drill.svg", "./assets/buildings/soldier.svg", "./assets/buildings/military.svg", "./assets/buildings/defense.svg", "./assets/buildings/medical.svg", "./assets/buildings/economy.svg", "./assets/buildings/default.svg", "./manifest.webmanifest", "./icon.svg", "./data/progression.json", "./data/resources.json"];
+const CACHE = "zroute-planner-v10";
+const ASSETS = ["./", "./index.html", "./i18n.js", "./assets/buildings/research.svg", "./assets/buildings/drill.svg", "./assets/buildings/soldier.svg", "./assets/buildings/military.svg", "./assets/buildings/warrior.svg", "./assets/buildings/defense.svg", "./assets/buildings/medical.svg", "./assets/buildings/economy.svg", "./assets/buildings/default.svg", "./manifest.webmanifest", "./icon.svg", "./data/progression.json", "./data/resources.json"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil((async () => {
   await Promise.all((await caches.keys()).filter(key => key.startsWith("zroute-planner-") && key !== CACHE).map(key => caches.delete(key)));
